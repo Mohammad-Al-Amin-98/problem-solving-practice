60bd9041f8b5a4a38b1de4f57cc2a3e3116dde0a
TheWizardPro <wannabeprogrammer007@yandex.com>
2023-06-21 06:49:41 +0600

    One more Easy Problem

---
+++ Anton_and_Danik.cpp
@@ -0,0 +1,18 @@
+//solution for codeforces problem Anton and Danik
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int a=0,d=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='A') a++;
+        else d++;
+    }
+    if(a>d) cout<<"Anton\n";
+    else if(d>a) cout<<"Danik\n";
+    else cout<<"Friendship\n";
+    return 0;
+}
